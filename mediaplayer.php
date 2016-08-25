c3e05bc16523421bb96e04687cc5477e4b9c299e
chris_blues <chris@musicchris.de>
2016-08-25 15:36:06 +0200

    Add page <title> to mediaplayer's header

--- mediaplayer.php
+++ mediaplayer.php
@@ -7,6 +7,7 @@ ini_set("display_errors", 0);
 <head>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
   <meta name=viewport content="width=device-width, initial-scale=1">
+  <title>cbPlayer - <?php echo $_GET["dir"]; ?></title>
   <meta name="author" content="chris_blues">
   <meta name="generator" content="brain 1.0 &amp; gedit 3.12.2 / kate 3.14.2">
   <link rel="stylesheet" href=".style.css" type="text/css">
